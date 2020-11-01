ff98169b5d6121044b3be3f4bd38c1d3394673aa
Piotr Loba <piotr.loba22@gmail.com>
2020-11-02 00:59:12 +0100

    Add products amount print

--- list.py
+++ list.py
@@ -16,3 +16,5 @@ for shop, product in shopping_list.items():
     amount = len(item)
   print(f"Idę do {shop.capitalize()}, kupuję tu następujące rzeczy: {products}.")
   products = []
+
+print(f"W sumie kupuję {amount} produktów.")
