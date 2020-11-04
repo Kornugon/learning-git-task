851de061ee5c49f7f7231e23db400d66e3ffab17
Piotr Loba <piotr.loba22@gmail.com>
2020-11-04 04:05:44 +0100

    Correct products list definition

--- list.py
+++ list.py
@@ -6,15 +6,13 @@ shopping_list = {
 "piekarnia": ["chleb", "bułki", "pączek"],
 "warzywniak": ["marchew", "seler", "rukola"]
 }
-products = []
 amount = 0
 
 for shop, product in shopping_list.items():
-  
+  products = []
   for item in product:
     products.append(item.capitalize())
     amount = len(item)
   print(f"Idę do {shop.capitalize()}, kupuję tu następujące rzeczy: {products}.")
-  products = []
 
 print(f"W sumie kupuję {amount} produktów.")
